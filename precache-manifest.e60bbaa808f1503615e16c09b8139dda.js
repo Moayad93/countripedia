7bb5047949a07867f9a425346b1625bafb0da4d1
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2019-11-06 16:21:29 +0300

    Updates

---
+++ precache-manifest.e60bbaa808f1503615e16c09b8139dda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1c62d32772939a2f5c4db4a38f83c3",
+    "url": "/countripedia/index.html"
+  },
+  {
+    "revision": "ddf39d20c8b27234bbeb",
+    "url": "/countripedia/static/css/main.3f067e82.chunk.css"
+  },
+  {
+    "revision": "427e3c7215cd6933baf0",
+    "url": "/countripedia/static/js/2.1564aab4.chunk.js"
+  },
+  {
+    "revision": "ddf39d20c8b27234bbeb",
+    "url": "/countripedia/static/js/main.b5453a3c.chunk.js"
+  },
+  {
+    "revision": "a366a9c733885f300103",
+    "url": "/countripedia/static/js/runtime-main.28cd0ef9.js"
+  }
+]);
